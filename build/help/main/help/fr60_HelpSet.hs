850a489258feb4c71228590b3c69a1a95bab1926
Darren Thompson <me@dtho.mp>
2022-03-24 12:55:37 -0400

    Add script to detect sysops. Fixup redirect calculations, cleaned up p-code

---
+++ build/help/main/help/fr60_HelpSet.hs
@@ -0,0 +1,27 @@
+<?xml version='1.0' encoding='ISO-8859-1' ?>
+<!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 2.0//EN" "http://java.sun.com/products/javahelp/helpset_2_0.dtd">
+
+<!-- HelpSet auto-generated on Mon Mar 21 12:19:06 EDT 2022 -->
+<helpset version="2.0">
+	<title>fr60 HelpSet</title>
+	<maps>
+		<mapref location="fr60_map.xml" />
+	</maps>
+	<view mergetype="javax.help.UniteAppendMerge">
+		<name>TOC</name>
+		<label>Ghidra Table of Contents</label>
+		<type>docking.help.CustomTOCView</type>
+		<data>fr60_TOC.xml</data>
+	</view>
+	<view>
+		<name>Search</name>
+		<label>Search for Keywords</label>
+		<type>docking.help.CustomSearchView</type>
+		<data engine="com.sun.java.help.search.DefaultSearchEngine">fr60_JavaHelpSearch</data>
+	</view>
+	<view>
+		<name>Favorites</name>
+		<label>Ghidra Favorites</label>
+		<type>docking.help.CustomFavoritesView</type>
+	</view>
+</helpset>
